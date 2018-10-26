6cf4f394617c670ba9f39aab36b10b1666e91f73
v5putting <957414954@qq.com>
2018-10-26 16:51:06 +0800

    about_nil

--- test.rb
+++ test.rb
@@ -1,13 +1,23 @@
-def a
-  # What happens when you call a method that doesn't exist.  The
-  # following begin/rescue/end code block captures the exception and
-  # makes some assertions about it.
-  begin
-    nil.jijuijuijui
-  rescue Exception => ex
-    # What exception has been caught?
-    puts ex
-  end
-end
+# def a
+#   # What happens when you call a method that doesn't exist.  The
+#   # following begin/rescue/end code block captures the exception and
+#   # makes some assertions about it.
+#   begin
+#     nil.jijuijuijui
+#   rescue Exception => ex
+#     # What exception has been caught?
+#     puts ex
+#   end
+# end
 
-a
+# def test (a = 1,b = 2)
+#   puts a.object_id
+#
+#   puts b.object_id
+# end
+# test
+def test2(a = :a, b = :a)
+  puts a.object_id
+  puts b.object_id
+end
+test2
